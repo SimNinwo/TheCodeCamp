7a14ae392cd008f99dcbd45c91693bf627310987
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-04-30 21:33:28 +0100

    Implementning URL versioning

--- TheCodeCamp/App_Start/WebApiConfig.cs
+++ TheCodeCamp/App_Start/WebApiConfig.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
 using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
 using Microsoft.Web.Http.Versioning;
 using Newtonsoft.Json.Serialization;
 
@@ -20,17 +22,27 @@ namespace TheCodeCamp
           cfg.DefaultApiVersion = new ApiVersion(1, 1);
           cfg.AssumeDefaultVersionWhenUnspecified = true;
           cfg.ReportApiVersions = true;
-          cfg.ApiVersionReader =ApiVersionReader.Combine(
-              new HeaderApiVersionReader("X-Version"),
-              new QueryStringApiVersionReader("ver"));
+          cfg.ApiVersionReader = new UrlSegmentApiVersionReader();
+              //ApiVersionReader.Combine(
+              //new HeaderApiVersionReader("X-Version"),
+              //new QueryStringApiVersionReader("ver"));
       });
 
       // Change case of Json
       config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
           new CamelCasePropertyNamesContractResolver();
 
+      var constraintResolver = new DefaultInlineConstraintResolver()
+      {
+          ConstraintMap =
+          {
+              ["apiVersion"] = typeof(ApiVersionRouteConstraint)
+          }
+      };
+
+
       // Web API routes
-      config.MapHttpAttributeRoutes();
+      config.MapHttpAttributeRoutes(constraintResolver);
 
       //config.Routes.MapHttpRoute(
       //    name: "DefaultApi",
