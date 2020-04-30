8594878c50e6acd2498cdfa034f08ea67d0f00e6
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-04-30 21:46:21 +0100

    using version conventions

--- TheCodeCamp/App_Start/WebApiConfig.cs
+++ TheCodeCamp/App_Start/WebApiConfig.cs
@@ -6,7 +6,9 @@ using System.Web.Http.Routing;
 using Microsoft.Web.Http;
 using Microsoft.Web.Http.Routing;
 using Microsoft.Web.Http.Versioning;
+using Microsoft.Web.Http.Versioning.Conventions;
 using Newtonsoft.Json.Serialization;
+using TheCodeCamp.Controllers;
 
 namespace TheCodeCamp
 {
@@ -26,6 +28,12 @@ namespace TheCodeCamp
               //ApiVersionReader.Combine(
               //new HeaderApiVersionReader("X-Version"),
               //new QueryStringApiVersionReader("ver"));
+
+          cfg.Conventions.Controller<TalksController>()
+              .HasApiVersion(1, 0)
+              .HasApiVersion(1,1)
+              .Action(m => m.Get(default(string), default(int), default(bool)))
+              .MapToApiVersion(2, 0);
       });
 
       // Change case of Json
